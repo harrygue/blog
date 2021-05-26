53d98263a39e457c0ee54855e11cbb6c48dd8464
harrygue <hg070817.gue@gmail.com>
2021-05-26 22:49:53 +0200

    add moderation and event sync on backend, make modifications on commentList

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,13 +1,24 @@
 import React from 'react'
 
 const CommentList = ({comments}) => {
-
+    const getComment = (comment) => {
+        switch(comment.status){
+            case 'approved':
+                return comment.content
+            case 'pending':
+                return 'Comment will be checked, be patient...'
+            case 'rejected':
+                return 'sorry, your comment was rejected ...'
+            default:
+                return 'Comment will be checked, be patient...'
+        }
+    }
     return (
         <div>
             <p>{comments && comments.length} comments</p>
             <ul>
                 {comments && comments.map(comment =>
-                    <li key={comment.id}>{comment.content}</li>)}
+                    <li key={comment.id}>{getComment(comment)}</li>)}
             </ul>
         </div>
     )
